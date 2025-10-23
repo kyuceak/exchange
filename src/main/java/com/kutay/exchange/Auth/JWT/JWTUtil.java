3462fece8c87f8c28ac2bf718434cd309ae9606a
kyuceak <kyuceak@sabanciuniv.edu>
2025-10-23 16:29:33 +0300

    JWTUtil implemented with generateToken Feature, validateToken feature

---
+++ src/main/java/com/kutay/exchange/Auth/JWT/JWTUtil.java
@@ -0,0 +1,43 @@
+package com.kutay.exchange.Auth.JWT;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String username, long expiryMinutes) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiryMinutes * 60 * 1000))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String validateTokenAndExtractUsername(String token) {
+        // maybe add try catch
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
