34a1bb51a5bfa43a97f5d7da949b5952fd258831
kyuceak <kyuceak@sabanciuniv.edu>
2025-12-16 13:38:39 +0300

    Wallet model refactored and wallet dtos implemented

--- src/main/java/com/kutay/exchange/modules/Wallet/Model/WalletEntity.java
+++ src/main/java/com/kutay/exchange/modules/Wallet/Model/WalletEntity.java
@@ -1,11 +1,13 @@
 package com.kutay.exchange.modules.Wallet.Model;
 
+import com.kutay.exchange.modules.Wallet.Model.enums.WalletStatus;
+import com.kutay.exchange.modules.Wallet.Model.enums.WalletType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.Instant;
+import java.util.Objects;
 
 @Entity
 @Table(name = "wallets")
@@ -19,27 +21,57 @@ public class WalletEntity {
             sequenceName = "wallet_id_seq")
     private Long id;
 
-//    @Column(nullable = false)
-//    private String walletAddress;
+    public WalletEntity() {
+    }
 
-//    @Column
-//    private BigDecimal balance; // BigDecimal --> exact decimal values,  not binary approximations like floating point.
-//
-//    @Column
-//    @Enumerated(EnumType.STRING) // enum --> fixed values
-//    private Currency currency;
+    private WalletEntity(Long customerId, WalletType walletType) {
+        this.customerId = Objects.requireNonNull(customerId);
+        this.walletType = Objects.requireNonNull(walletType);
+        this.walletStatus = WalletStatus.ACTIVE;
+    }
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private Long customerId;
 
     @Enumerated(EnumType.STRING)
-    private WalletType type;
+    @Column(nullable = false)
+    private WalletStatus walletStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private WalletType walletType;
 
     @Column(nullable = false)
-    private LocalDateTime created_at = LocalDateTime.now();
+    private Instant created_at;
 
     @Column(nullable = false)
-    private LocalDateTime updated_at = LocalDateTime.now();
+    private Instant updated_at;
 
+    @PrePersist
+    protected void onCreate() {
+        this.created_at = Instant.now();
+        this.updated_at = Instant.now();
+    }
 
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_at = Instant.now();
+    }
+
+    public void freeze() {
+        if (walletStatus == WalletStatus.CLOSED) {
+            throw new IllegalStateException("Closed wallet cannot be frozen");
+        } else if (walletStatus == WalletStatus.FROZEN) {
+            throw new IllegalStateException("Wallet is already frozen.");
+        }
+        walletStatus = WalletStatus.FROZEN;
+    }
+
+    public void unfreeze() {
+        if (walletStatus != WalletStatus.FROZEN) {
+            throw new IllegalStateException("Wallet is not frozen");
+        }
+
+        walletStatus = WalletStatus.ACTIVE;
+    }
 }
